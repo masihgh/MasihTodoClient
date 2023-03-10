fbc8ddc587ddef0ee3971e2811bcedc5a45d3e6e
masihgh <masihprogrammer@gmail.com>
2023-03-10 22:57:35 +0330

    Create BaseOf React Router Dom & Header & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import './assets/sass/App.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  
+  return (
+    <>
+      <BrowserRouter>
+      <Header />
+       <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="*" element={<NotFound /> }/> */}
+       </Routes>
+      <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App
